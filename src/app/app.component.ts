d014c9c9e1f7eb7d74accc0ea055581d1695b797
rathoddhruv <dhruvthenorthstar@gmail.com>
2020-06-25 16:01:13 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,13 +51,14 @@ export class AppComponent implements OnInit {
     // let weatherSeries = chart.series.push(new am4charts.LineSeries());
     this.zone.runOutsideAngular(() => {
       chart.cursor = new XYCursor();
-      chart.cursor.behavior = "panX";
+      chart.cursor.behavior = "selectXY";
 
       chart.scrollbarX = new am4charts.XYChartScrollbar();
       chart.scrollbarX.series.push(consumptionAxis);
 
       consumptionSeries.dataFields.valueY = "value";
       consumptionSeries.dataFields.dateX = "time";
+      consumptionSeries.columns.template.cursorOverStyle = MouseCursorStyle.pointer;
 
       chart.data = this.generateChartData();
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
